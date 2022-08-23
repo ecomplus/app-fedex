ba75d979e2b01de89b2d9af4cd14e158c2f9d42e
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2022-08-23 00:17:19 -0300

    chore: test insert iso date to updated

--- functions/lib/fedex/create-access.js
+++ functions/lib/fedex/create-access.js
@@ -31,11 +31,12 @@ const AxiosOrToken = (resolve, reject, clienId, clientSecret, isSandbox, storeId
         authenticate(resp.access_token, resolve)
         if (documentRef) {
           console.log('Atualizar token')
+          const date = new Date()
           documentRef.set(
             { 
               ...resp, 
               isSandbox,
-              updatedAt: new Date()
+              updated: date.toISOString()
             }
           ).catch(console.error)
         }
@@ -47,7 +48,7 @@ const AxiosOrToken = (resolve, reject, clienId, clientSecret, isSandbox, storeId
     documentRef.get()
       .then((documentSnapshot) => {
         const data = documentSnapshot.data() || null
-        const updateTime = data.updated_at.toDate() || '2022-08-22T10:24:49.950Z'
+        const updateTime = data.updated || '2022-08-22T10:24:49.950Z'
         if (documentSnapshot.exists &&
           Date.now() - new Date(updateTime).getTime() <= 50 * 60 * 1000 // access token expires in 50 minutes
         ) {
